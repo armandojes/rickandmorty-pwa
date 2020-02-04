2038373620858240b623bba017ceac232177a414
Jesusmaster <armandodejesus678@gmail.com>
2020-02-04 11:48:03 -0600

    header add link

--- source/app/components/header/index.jsx
+++ source/app/components/header/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Container from 'components/container'
 import style from './style'
+import { Link } from 'react-router-dom'
 
 const Header = () => (
   <header className={style.header}>
     <Container className={style.content}>
-      <img className={style.logo} src='/public/logo.png' />
+      <Link to='/'>
+        <img className={style.logo} src='/public/logo.png' />
+      </Link>
     </Container>
   </header>
 )
